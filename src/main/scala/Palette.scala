918568ca9d959db68eb115628ba20954dd43cb0a
Dustin Chaloupka <zadeta656@yahoo.com>
2016-04-02 14:11:24 -0500

    Fix colors to be RGB instead of RBG

--- src/main/scala/Palette.scala
+++ src/main/scala/Palette.scala
@@ -3,8 +3,8 @@ package org.chaloupka.lwjgl
 object Palette {
   sealed trait Color {
     def red: Int
-    def blue: Int
     def green: Int
+    def blue: Int
     def alpha: Float
 
     val minColorValue = 0
@@ -18,33 +18,33 @@ object Palette {
     }
 
     def floatValues: (Float, Float, Float, Float) = {
-      (inRangeFloat(red), inRangeFloat(blue), inRangeFloat(green), alpha)
+      (inRangeFloat(red), inRangeFloat(green), inRangeFloat(blue), alpha)
     }
   }
 
   case class White() extends Color {
     lazy val red = maxColorValue
-    lazy val blue = maxColorValue
     lazy val green = maxColorValue
+    lazy val blue = maxColorValue
     lazy val alpha = 1f
   }
 
   case class Black() extends Color {
     lazy val red = minColorValue
-    lazy val blue = minColorValue
     lazy val green = minColorValue
+    lazy val blue = minColorValue
     lazy val alpha = 1f
   }
 
   case class GrayA(alpha: Float) extends Color {
     lazy val red = minColorValue
-    lazy val blue = minColorValue
     lazy val green = minColorValue
+    lazy val blue = minColorValue
   }
 
-  case class RedBlueGreenA(red: Int, blue: Int, green: Int, alpha: Float) extends Color
-  case class RedBlueA(red: Int, blue: Int, alpha: Float) extends Color {
-    lazy val green = minColorValue
+  case class RedGreenBlueA(red: Int, green: Int, blue: Int, alpha: Float) extends Color
+  case class RedBlueA(red: Int, green: Int, alpha: Float) extends Color {
+    lazy val blue = minColorValue
   }
 
   case class RedGreenA(red: Int, green: Int, alpha: Float) extends Color {
@@ -52,17 +52,17 @@ object Palette {
   }
 
   case class RedA(red: Int, alpha: Float) extends Color {
+    lazy val green = minColorValue
     lazy val blue = minColorValue
-    lazy val green = minColorValue
   }
 
-  case class BlueGreenA(blue: Int, green: Int, alpha: Float) extends Color {
+  case class GreenBlueA(green: Int, blue: Int, alpha: Float) extends Color {
     lazy val red = minColorValue
   }
 
-  case class BlueA(blue: Int, alpha: Float) extends Color {
+  case class BlueA(green: Int, alpha: Float) extends Color {
     lazy val red = minColorValue
-    lazy val green = minColorValue
+    lazy val blue = minColorValue
   }
 
   case class GreenA(green: Int, alpha: Float) extends Color {
@@ -70,7 +70,7 @@ object Palette {
     lazy val blue = minColorValue
   }
 
-  case class RedBlueGreen(red: Int, blue: Int, green: Int) extends Color {
+  case class RedGreenBlue(red: Int, green: Int, blue: Int) extends Color {
     lazy val alpha = 1f
   }
 
@@ -85,19 +85,19 @@ object Palette {
   }
 
   case class Red(red: Int) extends Color {
+    lazy val green = minColorValue
     lazy val blue = minColorValue
-    lazy val green = minColorValue
     lazy val alpha = 1f
   }
 
-  case class BlueGreen(blue: Int, green: Int) extends Color {
+  case class GreenBlue(green: Int, blue: Int) extends Color {
     lazy val red = minColorValue
     lazy val alpha = 1f
   }
 
-  case class Blue(blue: Int) extends Color {
+  case class Blue(green: Int) extends Color {
     lazy val red = minColorValue
-    lazy val green = minColorValue
+    lazy val blue = minColorValue
     lazy val alpha = 1f
   }
 
